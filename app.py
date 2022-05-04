f229485819dbf8ea3bebffa66bdc79ac772dcd48
pokmongkol <tapok23@gmail.com>
2022-05-04 22:42:58 +0700

    read video and move robot complete

---
+++ app.py
@@ -0,0 +1,122 @@
+import cv2
+import numpy as np
+import socket
+import time
+import math
+
+
+def moveUR3(x, y):
+
+    HOST = "192.168.1.51"
+    PORT = 30002
+
+    count = 0
+    home = "movej(["+str(math.radians(0))+","+str(math.radians(-90))+","+str(math.radians(0))+","+str(math.radians(-90))+","+str(math.radians(0))+","+str(math.radians(0))+"], a=1.0, v=0.5)"
+    p1 = "movej(["+str(math.radians(5.83))+","+str(math.radians(-121.11))+","+str(math.radians(-23.77))+","+str(math.radians(-122.80))+","+str(math.radians(90.66))+","+str(math.radians(50.14))+"], a=1.0, v=50)"
+    p2 = "movej(["+str(math.radians(0.06))+","+str(math.radians(-101.28))+","+str(math.radians(-102.76))+","+str(math.radians(-64.08))+","+str(math.radians(91.77))+","+str(math.radians(176.95))+"], a=1.0, v=50)"
+    p3 = "movel(["+str(math.radians(5.83))+","+str(math.radians(-121.11))+","+str(math.radians(-23.77))+","+str(math.radians(-122.80))+","+str(math.radians(90.66))+","+str(math.radians(50.14))+"], a=1.0, v=50)"
+
+    cx = 0+(x-97)*(0.165/346)
+    cy = 0+(y-2)*(0.135/287)
+    print(cx)
+    print(cy)
+    move_xy = "movel(pose_trans(get_forward_kin(), p["+str(cx)+","+str(cy)+",0,0,0,0]), a=1.0, v=1)"
+    push_down = "movel(pose_trans(get_forward_kin(), p[0,0,0.09,0,0,0]), a=1.0, v=1)"
+    push_up = "movel(pose_trans(get_forward_kin(), p[0,0,-0.09,0,0,0]), a=1.0, v=1)"
+
+    while (count < 1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))
+        time.sleep(0.5)
+
+        s.send(("set_digital_out(1,True)"+"\n").encode('utf8'))
+        time.sleep(0.1)
+        s.send(("set_digital_out(2,True)"+"\n").encode('utf8'))
+        time.sleep(0.5)
+
+        # s.send((home+"\n").encode('utf8'))
+        # time.sleep(5)
+
+        s.send((p1+"\n").encode('utf8'))
+        time.sleep(3)
+
+        s.send((p2+"\n").encode('utf8'))
+        time.sleep(3)
+
+        s.send((move_xy+"\n").encode('utf8'))
+        time.sleep(2)
+        s.send((push_down+"\n").encode('utf8'))
+        time.sleep(10)
+        s.send((push_up+"\n").encode('utf8'))
+        time.sleep(2)
+
+        s.send((p3+"\n").encode('utf8'))
+        time.sleep(3)
+
+        s.send(("set_digital_out(1,False)"+"\n").encode('utf8'))
+        time.sleep(0.1)
+        s.send(("set_digital_out(2,False)"+"\n").encode('utf8'))
+        time.sleep(0.1)
+        count = count + 1
+
+        time.sleep(1)
+        data = s.recv(1024)
+        s.close()
+        print("Received", repr(data))
+
+cap = cv2.VideoCapture()
+cap.open(1, cv2.CAP_DSHOW)
+
+while(cap.read()):
+
+    ref,frame = cap.read()
+    roi = frame[:1080, 0:1920]
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_blur = cv2.GaussianBlur(gray, (15,15), 0)
+    thresh = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 1)
+    kernel = np.ones((3,3), np.uint8) # [[1 1 1],[1 1 1],[1 1 1]]
+    # closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations = 4)
+
+    # result_img = closing.copy()
+    result_img = thresh.copy()
+    contours,hierachy = cv2.findContours(result_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    x = 0
+    y = 0
+    counter = 0
+    for cnt in contours:
+        # FIND CIRCLE CONTOUR
+        x,y,w,h = cv2.boundingRect(cnt)
+        aspect_ratio = float(w)/h
+        if (aspect_ratio > 1.04 or aspect_ratio < 0.96):
+            continue
+        # FIND CIRCLE REQUIRED SIZE
+        area = cv2.contourArea(cnt)
+        if area < 20000 or area > 24000:
+            continue
+
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.putText(roi, str(x)+","+str(y), (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 2)
+        btn = cv2.waitKey(1)
+        if (btn == 32):
+            moveUR3(x, y)
+            cv2.destroyAllWindows()
+
+        # DRAW CIRCLE TO CONTOUR
+        ellipse = cv2.fitEllipse(cnt)
+        cv2.ellipse(roi, ellipse, (0, 255, 0), 2)
+        counter+=1
+
+    cv2.putText(roi, str(counter), (10,100), cv2.FONT_HERSHEY_SIMPLEX, 4, (255,0,0), 2, cv2.LINE_AA)
+    cv2.imshow("FIBO - FRA631 Class Project 2022", roi)
+
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
--- fibo-ur3.py
+++ fibo-ur3.py
@@ -1,25 +1,26 @@
-import numpy
 import socket
 import time
 import math
-# import cv2
 
 HOST = "192.168.1.51"
 PORT = 30002
 
 count = 0
 home = "movej(["+str(math.radians(0))+","+str(math.radians(-90))+","+str(math.radians(0))+","+str(math.radians(-90))+","+str(math.radians(0))+","+str(math.radians(0))+"], a=1.0, v=0.5)"
-p1 = "movej(["+str(math.radians(6.21))+","+str(math.radians(-122.02))+","+str(math.radians(-22.10))+","+str(math.radians(-121.38))+","+str(math.radians(90.09))+","+str(math.radians(50.65))+"], a=1.0, v=1)"
-p2 = "movej(["+str(math.radians(6.10))+","+str(math.radians(-114.05))+","+str(math.radians(-84.34))+","+str(math.radians(-67.12))+","+str(math.radians(90.21))+","+str(math.radians(50.80))+"], a=1.0, v=1)"
+p1 = "movej(["+str(math.radians(5.83))+","+str(math.radians(-121.11))+","+str(math.radians(-23.77))+","+str(math.radians(-122.80))+","+str(math.radians(90.66))+","+str(math.radians(50.14))+"], a=1.0, v=1)"
+p2 = "movej(["+str(math.radians(0.06))+","+str(math.radians(-101.28))+","+str(math.radians(-102.76))+","+str(math.radians(-64.08))+","+str(math.radians(91.77))+","+str(math.radians(176.95))+"], a=1.0, v=1)"
+p3 = "movel(["+str(math.radians(5.83))+","+str(math.radians(-121.11))+","+str(math.radians(-23.77))+","+str(math.radians(-122.80))+","+str(math.radians(90.66))+","+str(math.radians(50.14))+"], a=1.0, v=1)"
 
-x = 31
-y = 289
-cx = 0.095+(x-31)*(-0.045/383)
-cy = -0.04+(y-289)*(0.215/-252)
+x = 277
+y = 163
+cx = 0+(x-97)*(0.165/346)
+cy = 0+(y-2)*(0.135/287)
+print(cx)
+print(cy)
 move_xy = "movel(pose_trans(get_forward_kin(), p["+str(cx)+","+str(cy)+",0,0,0,0]), a=1.0, v=1)"
-push_down = "movel(pose_trans(get_forward_kin(), p[0,0,0.05,0,0,0]), a=1.0, v=1)"
+push_down = "movel(pose_trans(get_forward_kin(), p[0,0,0.07,0,0,0]), a=1.0, v=1)"
+push_up = "movel(pose_trans(get_forward_kin(), p[0,0,-0.07,0,0,0]), a=1.0, v=1)"
 
-print(p1)
 while (count < 1):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
@@ -31,22 +32,26 @@ while (count < 1):
     time.sleep(2)
     # s.send((home+"\n").encode('utf8'))
     # time.sleep(5)
-    # s.send((p1+"\n").encode('utf8'))
-    # time.sleep(3)
-    s.send((p2+"\n").encode('utf8'))
-    time.sleep(3)
-    s.send((move_xy+"\n").encode('utf8'))
-    time.sleep(5)
+    s.send((p1+"\n").encode('utf8'))
+    time.sleep(4)
 
-    # s.send((move_x+"\n").encode('utf8'))
-    # time.sleep(5)
+    s.send((p2+"\n").encode('utf8'))
+    time.sleep(4)
+    s.send((move_xy+"\n").encode('utf8'))
+    time.sleep(3)
+    s.send((push_down+"\n").encode('utf8'))
+    time.sleep(5)
+    s.send((push_up+"\n").encode('utf8'))
+    time.sleep(3)
+
+    s.send((p3+"\n").encode('utf8'))
+    time.sleep(4)
 
     s.send(("set_digital_out(1,False)"+"\n").encode('utf8'))
     time.sleep(0.1)
     s.send(("set_digital_out(2,False)"+"\n").encode('utf8'))
     time.sleep(0.1)
     count = count + 1
-    print(count)
 
     time.sleep(1)
     data = s.recv(1024)
