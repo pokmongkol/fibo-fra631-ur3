eb33062708d4c8eaccab5adf78e4b3079e907ee7
pokmongkol <tapok23@gmail.com>
2022-05-04 15:19:10 +0700

    opencv

--- fibo-ur3.py
+++ fibo-ur3.py
@@ -9,12 +9,17 @@ PORT = 30002
 
 count = 0
 home = "movej(["+str(math.radians(0))+","+str(math.radians(-90))+","+str(math.radians(0))+","+str(math.radians(-90))+","+str(math.radians(0))+","+str(math.radians(0))+"], a=1.0, v=0.5)"
-command0 = "movej(["+str(math.radians(2.68))+","+str(math.radians(-89.39))+","+str(math.radians(-91.44))+","+str(math.radians(-85.94))+","+str(math.radians(87.86))+","+str(math.radians(-25.47))+"], a=1.0, v=1)"
-command11 = "movej(["+str(math.radians(5.02))+","+str(math.radians(-175.87))+","+str(math.radians(-0.5))+","+str(math.radians(-93.03))+","+str(math.radians(90.36))+","+str(math.radians(-24.07))+"], a=1.0, v=1.5)"
-command12 = "movej(["+str(math.radians(24.06))+","+str(math.radians(-166.27))+","+str(math.radians(-20.97))+","+str(math.radians(-82.15))+","+str(math.radians(90.17))+","+str(math.radians(-4.97))+"], a=1.0, v=1.5)"
-command2 = "movel(pose_trans(get_forward_kin(), p[0,0,0.05,0,0,0]), a=1.0, v=1)"
+p1 = "movej(["+str(math.radians(6.21))+","+str(math.radians(-122.02))+","+str(math.radians(-22.10))+","+str(math.radians(-121.38))+","+str(math.radians(90.09))+","+str(math.radians(50.65))+"], a=1.0, v=1)"
+p2 = "movej(["+str(math.radians(6.10))+","+str(math.radians(-114.05))+","+str(math.radians(-84.34))+","+str(math.radians(-67.12))+","+str(math.radians(90.21))+","+str(math.radians(50.80))+"], a=1.0, v=1)"
 
-print(command0)
+x = 31
+y = 289
+cx = 0.095+(x-31)*(-0.045/383)
+cy = -0.04+(y-289)*(0.215/-252)
+move_xy = "movel(pose_trans(get_forward_kin(), p["+str(cx)+","+str(cy)+",0,0,0,0]), a=1.0, v=1)"
+push_down = "movel(pose_trans(get_forward_kin(), p[0,0,0.05,0,0,0]), a=1.0, v=1)"
+
+print(p1)
 while (count < 1):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
@@ -24,27 +29,17 @@ while (count < 1):
     time.sleep(0.1)
     s.send(("set_digital_out(2,True)"+"\n").encode('utf8'))
     time.sleep(2)
-    s.send((home+"\n").encode('utf8'))
-    time.sleep(5)
-    s.send((command0+"\n").encode('utf8'))
+    # s.send((home+"\n").encode('utf8'))
+    # time.sleep(5)
+    # s.send((p1+"\n").encode('utf8'))
+    # time.sleep(3)
+    s.send((p2+"\n").encode('utf8'))
     time.sleep(3)
-    s.send((command11+"\n").encode('utf8'))
-    time.sleep(3)
-    s.send((command2+"\n").encode('utf8'))
-    time.sleep(5)
-    s.send((command11+"\n").encode('utf8'))
-    time.sleep(3)
-    s.send((command0+"\n").encode('utf8'))
+    s.send((move_xy+"\n").encode('utf8'))
     time.sleep(5)
 
-    s.send((command12+"\n").encode('utf8'))
-    time.sleep(3)
-    s.send((command2+"\n").encode('utf8'))
-    time.sleep(5)
-    s.send((command12+"\n").encode('utf8'))
-    time.sleep(3)
-    s.send((command0+"\n").encode('utf8'))
-    time.sleep(3)
+    # s.send((move_x+"\n").encode('utf8'))
+    # time.sleep(5)
 
     s.send(("set_digital_out(1,False)"+"\n").encode('utf8'))
     time.sleep(0.1)
